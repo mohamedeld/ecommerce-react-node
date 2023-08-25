e158250aa051afaafa66db5428b1ab9c160a5185
mohamedeld <mohamed.azoz20010@gmail.com>
2023-08-25 18:03:20 +0300

    change app to index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
-const app = require("./app");
+const express = require("express");
 const connectDatabase = require("./db/Database");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+const compression = require("compression");
+const securityProviderMW = require("./middleware/security-provider.mw");
+const ErrorHandler = require("./middleware/error");
+const routes = require("./routes");
+
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
@@ -17,6 +27,18 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 // connect db
 connectDatabase();
 
+const app = express();
+app.use("/", express.static(path.join(__dirname, "./uploads")));
+
+app.use(compression());
+app.use(securityProviderMW);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(routes);
+app.use(ErrorHandler);
 // create server
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
