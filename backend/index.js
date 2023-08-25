dc6e0f3c83ad23b15cc4219224ab1e4d66e32375
mohamedeld <mohamed.azoz20010@gmail.com>
2023-08-25 17:54:11 +0300

    change some files

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
-const morgan = require("morgan");
-const compression = require('compression')
-const securityProviderMW = require("./middleware/security-provider.mw");
-const ErrorHandler = require("./middleware/error");
-const routes = require("./routes");
+const app = require("./app");
+const connectDatabase = require("./db/Database");
+const dotenv = require("dotenv");
+// Handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for handling uncaught exception`);
+});
 
 // config
-
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "config/.env",
   });
 }
 
-const app = express();
-app.use("/", express.static(path.join(__dirname, "./uploads")));
+// connect db
+connectDatabase();
 
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
 
-app.use(compression());
-app.use(securityProviderMW);
-app.use(express.json());
-app.use(cookieParser());
-app.use(morgan("dev"));
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(routes);
-app.use(ErrorHandler);
-module.exports = app;
+  server.close(() => {
+    process.exit(1);
+  });
+});
